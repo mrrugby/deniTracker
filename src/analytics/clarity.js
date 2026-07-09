f79e2afb18fec20175d993f479a2a005219b6bf6
mrrugby <snjishaka@gmail.com>
2026-07-09 17:27:48 +0300

    added clarity

---
+++ src/analytics/clarity.js
@@ -0,0 +1,5 @@
+import Clarity from '@microsoft/clarity'
+
+Clarity.init(
+    import.meta.env.VITE_CLARITY_PROJECT_ID
+)
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App.vue';
 import router from './router';
+import './analytics/clarity';
 import './style.css';
 
 registerSW({ immediate: true });
