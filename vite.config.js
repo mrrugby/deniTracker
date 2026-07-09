cb8edeb29f6e0fd39defd2c4e369da437d506a20
mrrugby <snjishaka@gmail.com>
2026-07-09 16:16:21 +0300

    fixed install flow

--- vite.config.js
+++ vite.config.js
@@ -18,18 +18,17 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait',
         start_url: '/',
+        scope: '/',
         icons: [
           {
             src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any'
           },
           {
             src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any'
           },
           {
             src: '/icons/maskable-512.png',
